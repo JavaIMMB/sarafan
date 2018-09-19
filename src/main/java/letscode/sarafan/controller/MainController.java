126b24f18108d552464f9e6651fcf726cfa5396d
JavaIMMB <electropo4ta2015@yandex.ru>
2018-09-19 17:22:37 +0300

    test commit

---
+++ src/main/java/letscode/sarafan/controller/MainController.java
@@ -0,0 +1,23 @@
+package letscode.sarafan.controller;
+
+import letscode.sarafan.domain.User;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user) {
+        HashMap<Object, Object> data = new HashMap<>();
+
+        data.put("profile", user);
+        data.put("messages", null);
+        model.addAttribute("frontendData", data);
+        return "main";
+    }
+}
